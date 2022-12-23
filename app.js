d2bfbb683cdd73455d828225162a277003016e9d
kimmand0o0 <yuwang0130@gmail.com>
2022-12-24 00:31:10 +0900

    :poop: 포트번호 변경 :poop:

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const PORT = 3001
+
 require('dotenv').config();
 
-app.listen(process.env.PORT, () => {
-    console.log(process.env.PORT, '포트로 서버가 열렸습니다.');
+app.listen(PORT, () => {
+    console.log(PORT, '포트로 서버가 열렸습니다.');
 });
 
 app.get('/', (req, res) => {
