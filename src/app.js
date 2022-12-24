1f204c6d46d7d661cc734271f360a9f5692e12ac
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2022-12-25 01:06:14 +0900

    :bug:fix : last check:bug:

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes');
+const cors = require('cors');
+const corsOption = {
+    origin: true,
+    withCredential: true,
+};
 
 require('dotenv').config();
+
+app.use(express.json());
+app.use(cors(corsOption));
+app.use('/', routes);
+
+const ErrorHandler = require('./middlewares/error.handler.middleware');
+app.use(ErrorHandler);
 
 app.listen(process.env.PORT, () => {
     console.log(process.env.PORT, '포트로 서버가 열렸습니다.');
 });
 
 app.get('/', (req, res) => {
-    res.send(`test6 ${process.env.PORT}`);
+    res.send(`last test ${process.env.PORT}`);
 });
