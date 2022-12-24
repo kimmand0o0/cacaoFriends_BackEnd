c09b1bf0679fea2682c170b93ef38b091c4721a1
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2022-12-24 23:17:53 +0900

    :bug:fix : env error:bug:

--- src/middlewares/error.handler.middleware.js
+++
@@ -1,7 +0,0 @@
-module.exports = (error, req, res, next) => {
-    console.error(error);
-    if (error.name.includes('Sequelize')) {
-        res.status(500).json({ errorMessage: 'Internal Server Error' });
-    }
-    res.status(error.status || 400).json({ errorMessage: error.message });
-};
