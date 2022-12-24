5cc0dd809259181b3a8cdbc6fe62871270defa32
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2022-12-24 13:33:07 +0900

    :green_heart:feat : 환경변수 추가

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const PORT = 3001
 
 require('dotenv').config();
 
-app.listen(PORT, () => {
-    console.log(PORT, '포트로 서버가 열렸습니다.');
+app.listen(process.env.PORT, () => {
+    console.log(process.env.PORT, '포트로 서버가 열렸습니다.');
 });
 
 app.get('/', (req, res) => {
-    res.send('안녕하세요, 저희는 항해 99 10기 E반 BE 김영재, 김혜주, 김혜란 입니다.');
+    res.send(`안녕하세요, 저희는 항해 99 10기 E반 BE 김영재, 김혜주, 김혜란 입니다. ${process.env.PORT}`);
 });
