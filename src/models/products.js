772e05264d6dbb9c22a490c6dc22aea715a540bf
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2022-12-26 09:52:38 +0900

    :bug:fix:오타 수정:bug:

--- src/models/products.js
+++ src/models/products.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
-            this.hasMany(models.Shots, {
+            this.hasMany(models.Shorts, {
                 as: 'Shorts',
                 foreignKey: 'productId',
             });
